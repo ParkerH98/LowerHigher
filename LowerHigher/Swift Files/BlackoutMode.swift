ca35f8e4a30faf968a8b7acdb2d489c992deec8c
Parker Hague <parkerhague@gmail.com>
2020-03-19 22:46:31 -0500

    Worked on BlackoutMode. Added UI & set up auto-layout.

---
+++ LowerHigher/Swift Files/BlackoutMode.swift
@@ -0,0 +1,27 @@
+//
+//  MainMenu.swift
+//  LowerHigher
+//
+//  Created by Parker Hague on 3/19/20.
+//  Copyright © 2019 Parker Hague. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import AVFoundation
+
+
+class BlackoutMode: UIViewController, AVAudioPlayerDelegate{
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
--- LowerHigher/Swift Files/MainMenu.swift
+++
@@ -1,9 +0,0 @@
-//
-//  MainMenu.swift
-//  LowerHigher
-//
-//  Created by Parker Hague on 6/29/19.
-//  Copyright © 2019 Parker Hague. All rights reserved.
-//
-
-import Foundation
